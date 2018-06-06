c5c71e075c6b6dc5a5bca8185466502c0a967a7b
anxu <axu.home@gmail.com>
2018-06-06 11:36:51 +0800

    增加统计信息内容，并增加错误详情脚本

--- scripts/operations.py
+++ scripts/operations.py
@@ -55,9 +55,11 @@ SQL = """
         s.date = "{date}";
 """
 
-SOLR_NODES = ["10.0.3.48:8983", "10.0.3.50:8983"]
+# SOLR_NODES = ["10.0.3.48:8983", "10.0.3.50:8983"]
+SOLR_NODES = ["10.163.89.73:8983"]
 SOLR_VERSION = "5.5.1"
 SOLR_COLLECTIONS = ["hengda_project"]
+SOLR_MAX_ROWS = 100000
 
 coll = SolrConnection(
     SOLR_NODES, version=SOLR_VERSION)[SOLR_COLLECTIONS[0]]
@@ -76,7 +78,11 @@ def sqlquery(sql, cur=None):
 
 def solrquery(query, coll=None):
     """ Solr 查询 """
-    return coll.search(query).result['response']
+    return coll.search(query).result["response"]
+
+
+def get_solrquery_numfound(query, coll=None):
+    return solrquery(query, coll)["numFound"]
 
 
 def groupby_row(rows):
@@ -91,12 +97,23 @@ def groupby_row(rows):
     return r
 
 
-def generate_solrquery_by_date(date):
+def solr_metrics_querys(date):
+    years, months, days = date[:4], date[4:6], date[6:8]
+    query = "years:{years} AND months:{months} AND days:{days}".format(
+        years=years, months=months, days=days)
     return {
-        "years": date[:4],
-        "months": date[4:6],
-        "days": date[6:8]
+        "total_number": query,
+        "plaintext_empty": "{0} AND -plaintext:*".format(query),
+        "total_errors": "{0} AND errors:*-*".format(query)
     }
+
+
+def solr_metrics(solr_metrics_querys):
+    r = {}
+    for (metric, query) in solr_metrics_querys.items():
+        r[metric] = get_solrquery_numfound({"q": query}, coll)
+
+    return r
 
 
 def transform_step(code):
@@ -122,8 +139,19 @@ def cli():
               default=time.strftime("%Y%m%d", time.localtime()),
               help="归集日期")
 def task(date):
-    query = generate_solrquery_by_date(date)
-    print(solrquery(query, coll))
+    """
+    获取归集任务信息脚本
+
+    样例：python operations.py task --date=20180606
+
+    注：若不指定日期则默认使用当前时间
+    """
+    solr_metrics_result = solr_metrics(solr_metrics_querys(date))
+    click.echo("索引总数: {0} | 空文本数量: {1} | 错误总数: {2}".format(
+        solr_metrics_result["total_number"],
+        solr_metrics_result["plaintext_empty"],
+        solr_metrics_result["total_errors"]
+    ))
 
     execute_sql = SQL.format(date=date).strip()
     tasks = groupby_row(sqlquery(execute_sql, cur))
@@ -139,6 +167,48 @@ def task(date):
                 table.add_row(step[1:])
 
         click.echo(table)
+
+
+@cli.command(short_help="查看空文本错误详情")
+@click.option("--date",
+              default=time.strftime("%Y%m%d", time.localtime()),
+              help="归集日期")
+def plaintext_empty_detail(date):
+    """
+    获取文本为空的语音详细信息
+
+    样例：python operations.py plaintext_empty_detail --date=20180606
+
+    注：若不指定日期则默认使用当前时间
+    """
+    query = {
+        "q": solr_metrics_querys(date)["plaintext_empty"],
+        "fl": set(["filename,errors"]),
+        "rows": SOLR_MAX_ROWS
+    }
+    for doc in solrquery(query, coll)["docs"]:
+        click.echo("{0},{1}".format(doc["filename"], doc["errors"]))
+
+
+@cli.command(short_help="所有错误详情")
+@click.option("--date",
+              default=time.strftime("%Y%m%d", time.localtime()),
+              help="归集日期")
+def errors_detail(date):
+    """
+    获取所有错误语音详细信息
+
+    样例：python operations.py errors_detail --date=20180606
+
+    注：若不指定日期则默认使用当前时间
+    """
+    query = {
+        "q": solr_metrics_querys(date)["total_errors"],
+        "fl": set(["filename,errors"]),
+        "rows": SOLR_MAX_ROWS
+    }
+    for doc in solrquery(query, coll)["docs"]:
+        click.echo("{0},{1}".format(doc["filename"], doc["errors"]))
 
 
 if __name__ == "__main__":
